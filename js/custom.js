dd2159432d0037b094edf84587855c42a1c5ad27
Chirag Modi <modichirag14@gmail.com>
2016-03-22 13:15:32 +0530

    initial release of slick vertical slider

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+
+function slickHeight() {
+  var winWidth = $(window).width();
+    if (winWidth > 767) {
+        var sliderHeight = $('.slick-slider').height();
+        console.log(sliderHeight);
+        $('#slick-pager .slick-pager').css('height', sliderHeight - 60);
+    }
+}
+$( window ).resize(function() {
+    slickHeight();
+});
+jQuery(document).ready(function($) {
+    $('.featuredPostSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        adaptiveHeight: true,
+        asNavFor: '.bx-pager'
+    });
+    $('.slick-pager').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: $('.pagerNavigation .bottom'),
+        prevArrow: $('.pagerNavigation .top'),
+        asNavFor: '.featuredPostSlider',
+        focusOnSelect: true,
+        vertical: true,
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                vertical: false
+            }
+        }]
+    });
+    slickHeight();
+});
+
